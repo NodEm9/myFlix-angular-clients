f0dc7926a74839f94bd95594e3bc9ba9798e53d8
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-09-15 17:31:00 +0200

    create filter component and implement movies search

---
+++ src/app/movie-filter/movie-filter.component.ts
@@ -0,0 +1,51 @@
+/**
+ * @name MovieFilterComponent 
+ * @summary: This component is responsible for the search bar that
+ *  filters the movies based on the user's input.
+ * @param searchQuery: string
+ * @param searchChange: EventEmitter 
+ * @method onSearchChange(): void
+ * @returns searchChange event
+ * @class MovieFilterComponent 
+ * @exports class MovieFilterComponent
+ */
+import { Component, Injectable, Output, EventEmitter } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+
+import { MovieCardComponent } from '../movie-card/movie-card.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+Injectable({
+  providedIn: 'root'
+})
+
+@Component({
+  selector: 'movie-filter',
+  standalone: true,
+  imports: [
+    MovieCardComponent,
+    MatIconModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgOptimizedImage,
+    FormsModule
+  ],
+  
+  templateUrl: './movie-filter.component.html',
+  styleUrl: './movie-filter.component.scss'
+})
+export class MovieFilterComponent {
+  searchQuery: string = '';
+  @Output() searchChange = new EventEmitter<string>();
+
+  onSearchChange(): void {
+    this.searchChange.emit(this.searchQuery);
+    this.searchQuery = '';
+  }
+
+}
